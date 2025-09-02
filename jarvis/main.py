c666d79a79a19863625ed5d8e82fd010e2458415
aman <aman1290k@gmail.com>
2025-09-02 17:17:33 +0530

    added few new things

--- jarvis/main.py
+++ jarvis/main.py
@@ -1,6 +1,7 @@
 import speech_recognition as sr
 import webbrowser
 import pyttsx3
+import musiclibrary
 
 
 recognizer = sr.Recognizer()
@@ -8,24 +9,48 @@ engine = pyttsx3.init()
 
 
 def speak(text): 
-    engine.say("helloooo")
+    engine.say("text")
     engine.runAndwait()
+
+    def processcommand(c):
+        if "open YouTube" in c.lower:
+            webbrowser.open("https://www.youtube.com")
+        elif "open Google" in c.lower:
+            webbrowser.open("https://www.google.com")
+        elif "open LinkedIn" in c.lower:
+            webbrowser.open("https://www.linkedin.com")
+        elif "open Instagram" in c.lower:
+            webbrowser.open("https://www.instagram.com")
+        elif "open facebook" in c.lower:
+            webbrowser.open("https://www.facebook.com")
+
+        elif c.lower().startswith("play") :
+            song = c.lower().split("")[1]         #c.lower = "song name here"
+            link = musiclibrary.music[song]   
+            webbrowser.open(link)                
+        else:
+            speak("Sorry, I didn't understand that command.")
 
     if __name__ == "__main__":
         speak("Hello, how can I assist you today?") 
 
-    recognizer = sr.Recognizer()
+    r = sr.Recognizer()
 
-# Use microphone as source
-with sr.Microphone() as source:
-    print("Listening...")
-    audio = r.listen(source)
+    print("recoznizing")
+    try:
+        with sr.Microphone() as source:
+         print("Listening...")
+         audio = r.listen(source,timeout=2,phrase_time_limit=2)        # Use microphone as source
+      
+        word = r.recognize_google(audio)          # Convert speech to text
+        if(word.lower()=="jarvis"):
+         speak("yes sir")                             # Respond to wake word
+            
+        with sr.Microphone() as source:
+         print("Listening...")
+         audio = r.listen(source )   
+         command = r.recognize_google(audio) 
 
-try:
-    # Convert speech to text
-    command = r.recognize_google(audio)
-    print(command)
-except sr.UnknownValueError:
-    print("Sorry, I could not understand the audio.")
-except sr.RequestError as e:
-    print(f"Could not request results; {e}")
+         processcommand(command)
+    except Exception as e:
+        print("error; {0}".format(e))
